046205db575343771c8ec6e0ad915c14710aeb4f
David Perez-Suarez <david.perez-suarez@fmi.fi>
2013-06-05 17:33:07 +0300

    added server variable to global for easy configuration

--- IMPExServer.php
+++ IMPExServer.php
@@ -2,7 +2,7 @@
 require_once('Zend/Soap/AutoDiscover.php');
 require_once('Zend/Soap/Server.php');
 require_once('Zend/Soap/Wsdl.php');
-require_once('library/IMPExMethods.php');
+require_once('IMPExMethods.php');
 $wsdl = new Zend_Soap_Autodiscover();
 $wsdl->setClass('IMPExMethods');
 if (isset($_GET['wsdl'])) 
@@ -11,7 +11,7 @@ if (isset($_GET['wsdl']))
   } 
 else 
   {
-    $server = new Zend_Soap_Server('http://localhost/IMPExServer.php?wsdl');
+    $server = new Zend_Soap_Server($server.'IMPExServer.php?wsdl');
     $server->setClass('IMPExMethods');
     $server->setEncoding('ISO-8859-1');
     $server->handle();
--- globals.php
+++ globals.php
@@ -1,4 +1,5 @@
 <?php
+$server = 'http://localhost/IMPEx/IMPEx-SMDB_php_webservice/';
 $institute = 'fmi'; /*latmos, .... */
 
 
