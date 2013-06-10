96e22e2a1354b377ceca19d1c188def75df2ffea
David Perez-Suarez <david.perez-suarez@fmi.fi>
2013-06-10 15:40:55 +0300

    template for latmos local functions added

---
+++ latmos/local_functions_latmos.php
@@ -0,0 +1,26 @@
+<?php
+
+$dict_functions = array('getDataPointValue' => 'run_getDataPointValue',
+			'getFieldLine' => 'run_getFieldLine',
+			'getDataPointValue_spacecraft' => 'run_getDataPointValue_spacecraft',
+			'getDataPointSpectra' => 'run_getDataPointSpectra',
+			'getSurface' => 'run_getSurface',
+			'getFileURL' => 'run_getFileURL',
+			'getDataPointSpectra_spacecraft' => 'run_getDataPointSpectra_spacecraft',
+			'getParticleTrajectory' => 'run_getParticleTrajectory');
+
+
+
+
+/**
+ *
+ */
+function run_getDataPointValue($ResourceID, $variables,
+			       $url_XYZ, $IMFClockAngle,
+			       $InterpolationMethod,
+			       $OutputFiletype){
+  return true;
+}
+
+
+?>
